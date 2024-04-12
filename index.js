04d616474567b1c2c2b25bce44f408f15e8f1861
MallamFarouk <farukissa010@gmail.com>
2024-04-12 01:03:44 -0700

    Portfolio getting form

--- index.js
+++ index.js
@@ -36,3 +36,49 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+document.getElementById('myForm').addEventListener('submit', function (e) {
+  e.preventDefault()
+  if(validateForm){
+    console.log('Submitted')
+    //this.submit()
+  }
+  
+})
+
+function validateForm(){
+  //fetch input
+  var nameInput = document.getElementById("name");
+  var messageInput = document.getElementById("message");
+  var emailInput = document.getElementById("email");
+
+  // Regular expressions for email validation
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+  // Flag to track validation status
+  var isValid = true;
+  
+  // Reset previous error messages
+  nameInput.setCustomValidity("");
+  emailInput.setCustomValidity("");
+  messageInput.setCustomValidity("");
+
+  // Validate name
+  if (nameInput.value.trim() === "") {
+    nameInput.setCustomValidity("Please enter your name.");
+    isValid = false;
+};
+// Validate email
+if (!emailRegex.test(emailInput.value.trim())) {
+  emailInput.setCustomValidity("Please enter a valid email address.");
+  isValid = false;
+}
+// Validate message
+if (messageInput.value.trim() === "") {
+  messageInput.setCustomValidity("Please enter your message.");
+  isValid = false;
+}
+
+// Return validation status
+return isValid;
+}
